28428fba102fb3ce4695772416f7a104bec84ba3
fattah-ab <fattaha.12@gmail.com>
2020-12-01 20:20:26 +0700

    edit answer

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function warnTheSheep(queue) {
     // your code here
-    let index = 0;
+    let numberOfSheep = 0;
    
     for (let i = queue.length - 1 ; i >= 0; i--){
         // return queue[i]
-        index++
-        // console.log(index)
+        numberOfSheep++
+        // console.log(numberOfSheep)
         if (queue[queue.length -1] === "wolf") {
           return "Pls go away and stop eating my sheep"
         } else if ( queue[i] === "wolf") {
-          // let index = [i]
-          return `Oi! Sheep number ${index - 1}! You are about to be eaten by a wolf!`
+          // let numberOfSheep = [i]
+          return `Oi! Sheep number ${numberOfSheep - 1}! You are about to be eaten by a wolf!`
         } 
         
       }
